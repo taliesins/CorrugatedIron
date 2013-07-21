91269d5824741e52b5be982eb8973abdb3c192e0
Taliesin Sisson <taliesins@yahoo.com>
2013-07-21 20:22:34 +0100

    Update version

--- GlobalAssemblyInfo.cs
+++ GlobalAssemblyInfo.cs
@@ -6,5 +6,5 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 
-[assembly: AssemblyVersion("1.3.2.2")]
-[assembly: AssemblyFileVersion("1.3.2.2")]
+[assembly: AssemblyVersion("1.4.0.1")]
+[assembly: AssemblyFileVersion("1.4.0.1")]
